06766c21230cb1d77834bdc899e4fd93f587a8a0
peppiranta <peppira@metropolia.fi>
2023-04-26 11:41:34 +0300

    taustakuvat

--- Nimo/main.js
+++ Nimo/main.js
@@ -1,2 +1,2 @@
 let nimi = prompt( "Syötä nimesi:");
-document.querySelector('#a').innerHTML = 'Hello ' + nimi + "Tervetuloa pelaamaan The seven wonders-peliä! ";
+document.querySelector('#a').innerHTML = 'Hello ' + nimi + "! Tervetuloa pelaamaan The seven wonders-peliä! ";
